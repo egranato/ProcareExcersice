770b93eead2a52e523509c71b3fd52c99b5e06ea
Ethan Granato <ethan@granato.net>
2024-01-31 14:33:30 -0700

    completed

---
+++ AddressValidationService.cs
@@ -0,0 +1,63 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="AddressValidationService.cs" company="Procare Software, LLC">
+//     Copyright © 2021-2024 Procare Software, LLC. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+namespace Procare.AddressValidation.Tester;
+
+using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+public class AddressValidationService : BaseHttpService
+{
+    public AddressValidationService(IHttpClientFactory httpClientFactory, bool disposeFactory, Uri baseUrl)
+        : this(httpClientFactory, disposeFactory, baseUrl, null, false)
+    {
+    }
+
+    protected AddressValidationService(IHttpClientFactory httpClientFactory, bool disposeFactory, Uri baseUrl, HttpMessageHandler? httpMessageHandler, bool disposeHandler)
+        : base(httpClientFactory, disposeFactory, baseUrl, httpMessageHandler, disposeHandler)
+    {
+    }
+
+    public async Task<string> GetAddressesAsync(AddressValidationRequest request, int attempts = 0)
+    {
+        // I wanted to use a retry policy but I wasn't sure how to implement that in a console application so I just did it manually
+        await Task.Delay((int)Math.Pow(2, attempts)).ConfigureAwait(false);
+
+        if (attempts > 3)
+        {
+            // the call has failed 3 times, time to abort
+            throw new ArgumentException("Address lookup has failed too many times and has been aborted");
+        }
+
+        using var cancellationTokenSource = new CancellationTokenSource();
+        cancellationTokenSource.CancelAfter(750);
+        var token = cancellationTokenSource.Token;
+
+        try
+        {
+            using var httpRequest = request.ToHttpRequest(this.BaseUrl);
+            using var response = await this.CreateClient().SendAsync(httpRequest, HttpCompletionOption.ResponseHeadersRead, token).ConfigureAwait(false);
+
+            if ((int)response.StatusCode >= 500)
+            {
+                // call got a 5xx response, retry and increment attempts counter
+                return await this.GetAddressesAsync(request, attempts + 1).ConfigureAwait(false);
+            }
+            else
+            {
+                response.EnsureSuccessStatusCode();
+
+                return await response.Content.ReadAsStringAsync(token).ConfigureAwait(false);
+            }
+        }
+        catch (OperationCanceledException)
+        {
+            // call took to long and got cancelled, retry and increment attempts counter
+            return await this.GetAddressesAsync(request, attempts + 1).ConfigureAwait(false);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="Program.cs" company="Procare Software, LLC">
+//     Copyright © 2021-2024 Procare Software, LLC. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+namespace Procare.AddressValidation.Tester;
+
+using System;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.CompilerServices;
+using System.Threading;
+using System.Threading.Tasks;
+
+internal sealed class Program
+{
+    [SuppressMessage("Design", "CA1031:Do not catch general exception types", Justification = "Main should not leak any exceptions.")]
+    private static async Task Main()
+    {
+        Uri addressValidationBaseUrl = new("https://addresses.dev-procarepay.com");
+
+        using HttpClientFactory factory = new();
+        using AddressValidationService addressService = new(factory, false, addressValidationBaseUrl);
+
+        // AddressValidationRequest request = new() { Line1 = "1 W Main", City = "Medford", StateCode = "OR", ZipCodeLeading5 = "97501" };
+        // AddressValidationRequest request = new();
+        AddressValidationRequest request = new() { Line1 = "1125 17th St Ste 1800", City = "Denver", StateCode = "CO", ZipCodeLeading5 = "80202" };
+
+        Stopwatch stopwatch = new();
+        for (int i = 0; i < 20; i++)
+        {
+            stopwatch.Restart();
+            try
+            {
+                var response = await addressService.GetAddressesAsync(request).ConfigureAwait(false);
+                Console.WriteLine(response);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+
+            Console.WriteLine(stopwatch.Elapsed);
+            Console.WriteLine();
+        }
+    }
+}
